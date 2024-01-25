5f835129e0494f501afc4c47e4c9901539507971
“acalderon20” <acalderon20@coe.edu>
2024-01-25 15:08:00 -0600

    Initial Commit

---
+++ cameraPractice/cameraPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  cameraPracticeApp.swift
+//  cameraPractice
+//
+//  Created by Adolfo Calderon on 1/25/24.
+//
+
+import SwiftUI
+
+@main
+struct cameraPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
